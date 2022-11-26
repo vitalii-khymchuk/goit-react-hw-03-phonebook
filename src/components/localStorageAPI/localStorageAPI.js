d6b620ac1ce25120f2416077c1b69571bb271460
Vitalii Khymchuk <vitalii.workbox001@gmail.com>
2022-11-26 21:30:31 +0200

    local storage debug

--- src/components/localStorageAPI/localStorageAPI.js
+++ src/components/localStorageAPI/localStorageAPI.js
@@ -1,11 +1,11 @@
-function update(item, data) {
+const update = (item, data) => {
   const stringified = JSON.stringify(data);
   localStorage.setItem(item, stringified);
-}
+};
 
-function get(item) {
+const get = item => {
   const savedData = localStorage.getItem(item);
   return JSON.parse(savedData);
-}
+};
 
 export default { update, get };
